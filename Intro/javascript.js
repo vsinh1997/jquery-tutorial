701d2ebda705ce7762b691a93920022a48850199
duongnc <duongnc@rikkeisoft.com>
2023-11-15 22:34:15 +0900

    first commit

---
+++ Intro/javascript.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+    // $('*').hide()
+    // $('span').css('color', 'red')
+
+    // $('ul#list li:first').css('color', 'red')
+    // $('ul#list li:last').css('color', 'green')
+    // $('ul#list li:even').css('background', 'yellow')
+    // $('ul#list li:odd').css('background', '#cccccc')
+    // $('ul#list li:nth-child(3n)').css('list-style', 'none')
+
+    // $(':button').hide()
+    // $(':submit').hide()
+    // $(':text').hide()
+
+    // $('[href]').css('color', 'red')
+    // $('a[href="https://www.youtube.com"]').css('color', 'green')
+
+})
